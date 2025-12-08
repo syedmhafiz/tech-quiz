4bc19faddf59d6ee6181752142919d7c5bec9470
Syed Mahmud Hafiz <64685964+syedmhafiz@users.noreply.github.com>
2025-12-08 13:54:06 +0600

    Dockerized the application (#2)
    * Dockerized the application
    
    * Updated readme

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,18 +2,22 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <div className='fixed bottom-0 w-full bg-gray-100 py-4'>
-        <h3 className='text-center text-sm'>
-          All rights reserved by <Link
-                to={`https://github.com/syedmhafiz`}
-                target="_blank"
-                style={
-                  { textDecoration: 'none',
-                    color: "black" }
-                }
-              >সৈ.মা.হা.</Link> © 2025 
-        </h3>
+      <h3 className='text-center text-sm'>
+        All rights reserved by <Link
+          to={`https://github.com/syedmhafiz`}
+          target="_blank"
+          style={
+            {
+              textDecoration: 'none',
+              color: "green"
+            }
+          }
+        >সৈ.মা.হা.</Link> © {currentYear}
+      </h3>
     </div>
   )
 }
